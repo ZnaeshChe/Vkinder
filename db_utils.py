5cd7addff39703ade97fd1d74969c3998f85dd0e
ZnaeshChe <129045333+ZnaeshChe@users.noreply.github.com>
2023-07-05 20:18:12 +0300

    db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,92 +1,64 @@
-"""
-Файл с описанием класса подклюения к БД
-"""
-# pylint: disable = import-error, line-too-long, logging-fstring-interpolation
 import logging
 import sys
 
-import psycopg2
+from sqlalchemy import create_engine, Column, Integer, ARRAY, inspect
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'new_users_2'
+
+    user_id = Column(Integer, primary_key=True)
+    searched_users = Column(ARRAY(Integer), nullable=False)
 
 
 class Saver:
-    """
-    Сохранение состояния пользователя
-    """
-
-    # pylint: disable=too-many-arguments
-    def __init__(self, connstr=None, database='user_data', user='admin', password='password',
-                 host='127.0.0.1', port=80, table='searched_users'):
-        """
-        Создает соединение с базой данных PostgreSQL.
-        """
+    def __init__(self, connstr=None):
         self.logger = logging.getLogger(__name__)
-        self.connection = None
-        self.table = table
-        try:
-            self.connection = psycopg2.connect(connstr) if connstr else psycopg2.connect(
-                database=database,
-                user=user,
-                password=password,
-                host=host,
-                port=port
-            )
-            self.check_table()
-            logging.info('Успешное подключение к базе данных!')
-        except psycopg2.Error as error:
-            logging.error(f"Ошибка при подключении к базе данных: {error}")
+        self.engine = create_engine(connstr)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+        self.table_check()
 
-    def create_table(self):
+    def table_create(self):
         """
         Создание таблицы, если она не существует.
         """
-        with self.connection.cursor() as cursor:
-            cursor.execute(
-                f"""
-                CREATE TABLE IF NOT EXISTS {self.table} (
-                    user_id INTEGER PRIMARY KEY,
-                    searched_users INTEGER[] NOT NULL
-                );
-                """
-            )
-            self.connection.commit()
+        Base.metadata.create_all(self.engine)
 
-    def check_table(self):
+    def table_check(self):
         """
         Проверка существования таблицы, создание при необходимости.
         """
-        with self.connection.cursor() as cursor:
-            cursor.execute(
-                "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = %s);",
-                (self.table,)
-            )
-            if not cursor.fetchone()[0]:
-                response = input('Создать базу? (Y/N): ').upper()
-                if response == 'Y':
-                    self.create_table()
-                    logging.info('Таблица создана, запускаю бота!')
-                else:
-                    print('Выхожу...')
-                    sys.exit(0)
+        inspector = inspect(self.engine)
+        if not inspector.has_table(User.__tablename__):
+            response = input('Таблицы не существует. Создать таблицу? (Y/N): ').upper()
+            if response == 'Y':
+                self.table_create()
+                self.logger.info('Таблица создана.')
             else:
-                logging.info('База существует, запускаю бота')
+                self.logger.info('Выход...')
+                sys.exit(0)
 
     def save_session_to_db(self, user_id, searched_users):
         """
         Сохраняет или обновляет сессию пользователя в базе данных.
         :param user_id:          ID пользователя ВКонтакте.
-        :param searched_users:   Найденные пользователи.
+        :param searched_users:   Найденный пользователь.
         """
-        with self.connection.cursor() as cursor:
-            cursor.execute(
-                f"""
-                INSERT INTO {self.table} (user_id, searched_users)
-                VALUES (%s, %s)
-                ON CONFLICT (user_id) DO UPDATE SET
-                    searched_users = {self.table}.searched_users || %s;
-                """,
-                (user_id, searched_users, searched_users)
-            )
-            self.connection.commit()
+        user = self.session.query(User).get(user_id)
+        if user:
+            user.searched_users = user.searched_users + searched_users
+            self.session.merge(user)
+        else:
+            user = User(user_id=user_id, searched_users=searched_users)
+            self.session.add(user)
+
+        self.session.flush()
+        self.session.commit()
 
     def get_user_data_from_db(self, user_id):
         """
@@ -94,8 +66,8 @@ class Saver:
         :param user_id: ID пользователя.
         :return:        Список найденных пользователей, либо None.
         """
-        with self.connection.cursor() as cursor:
-            cursor.execute(f"SELECT searched_users FROM {self.table} WHERE user_id = %s;", (user_id,))
-            result = cursor.fetchone()
-
-        return result[0] if result else []
+        user = self.session.query(User).get(user_id)
+        if user:
+            return user.searched_users
+        else:
+            return []
